ee1dfa5ae6fe262fd51d63db7b427c5ea19677ec
Ruy Medeiros <ruymedeiros09@gmail.com>
2025-07-01 18:33:17 -0300

    Adicionado schemas e criado uma rota no routes

--- routes/jogador.py
+++ routes/jogador.py
@@ -7,3 +7,7 @@ jogador_router = APIRouter()
 @jogador_router.get("/")
 async def inicio():
     return "Bem vindo ao fullstack farm"
+
+@jogador_router.get("/jogadores")
+async def lista_jogadores():
+    return conexao.local.jogador.find()
