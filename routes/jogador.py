60e28fd604c3ec30e2da150f95772811c9ac89f5
Ruy Medeiros <ruymedeiros09@gmail.com>
2025-07-16 21:34:37 -0300

    Adicionado a funcionalidade de atualizar um jogador existente com base no id

--- routes/jogador.py
+++ routes/jogador.py
@@ -6,16 +6,21 @@ from bson import ObjectId
 
 jogador_router = APIRouter()
 
+
 @jogador_router.get('/')
 async def inicio():
     return "Bem vindo ao fullstack farm"
 
-#lista todos os jogadores
+# lista todos os jogadores
+
+
 @jogador_router.get('/jogadores')
 async def lista_jogadores():
     return listaJogadoresEntidade(conexao.local.jogador.find())
 
-#Detalhes de um jogador
+# Detalhes de um jogador
+
+
 @jogador_router.get('/jogadores/{jogador_id}')
 def buscar_jogador_id(jogador_id):
     return jogadorEntidade(
@@ -25,8 +30,31 @@ def buscar_jogador_id(jogador_id):
         )
     )
 
-#insere novos jogadores
+# insere novos jogadores
+
+
 @jogador_router.post('/jogadores')
 async def cadastra_jogadores(jogador: Jogador):
     conexao.local.jogador.insert_one(dict(jogador))
     return listaJogadoresEntidade(conexao.local.jogadores.find())
+
+# atualiza jogador
+
+
+@jogador_router.put('/jogadores/{jogador_id}')
+async def atualiza_jogador(jogador_id, jogador: Jogador):
+    conexao.local.jogador.find_one_and_update(
+        {
+            "_id": ObjectId(jogador_id)
+        },
+        {
+            "$set": dict(jogador)
+        }
+    )
+    return jogadorEntidade(
+        conexao.local.jogador.find_one(
+            {
+                "_id": ObjectId(jogador_id)
+            }
+        )
+    )
