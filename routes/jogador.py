67ee427992e26029eb8be76497c8873fa751b74f
Ruy Medeiros <ruymedeiros09@gmail.com>
2025-07-16 21:59:57 -0300

    Criando CORS para que a API possa se comunicar com React

--- routes/jogador.py
+++ routes/jogador.py
@@ -12,15 +12,11 @@ async def inicio():
     return "Bem vindo ao fullstack farm"
 
 # lista todos os jogadores
-
-
 @jogador_router.get('/jogadores')
 async def lista_jogadores():
     return listaJogadoresEntidade(conexao.local.jogador.find())
 
 # Detalhes de um jogador
-
-
 @jogador_router.get('/jogadores/{jogador_id}')
 def buscar_jogador_id(jogador_id):
     return jogadorEntidade(
@@ -31,16 +27,12 @@ def buscar_jogador_id(jogador_id):
     )
 
 # insere novos jogadores
-
-
 @jogador_router.post('/jogadores')
 async def cadastra_jogadores(jogador: Jogador):
     conexao.local.jogador.insert_one(dict(jogador))
     return listaJogadoresEntidade(conexao.local.jogadores.find())
 
 # atualiza jogador
-
-
 @jogador_router.put('/jogadores/{jogador_id}')
 async def atualiza_jogador(jogador_id, jogador: Jogador):
     conexao.local.jogador.find_one_and_update(
@@ -60,8 +52,6 @@ async def atualiza_jogador(jogador_id, jogador: Jogador):
     )
 
 # exclui jogador
-
-
 @jogador_router.delete('/jogadores/{jogador_id}')
 async def exclui_jogador(jogador_id):
     return jogadorEntidade(
